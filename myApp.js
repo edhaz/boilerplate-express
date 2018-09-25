b8f6b7b57c55d14c279dfbd3fd22ac61f4484013
Ed Hazledine <ed.hazledine@gmail.com>
2018-09-25 21:24:46 +0100

    Exercise 2

--- myApp.js
+++ myApp.js
@@ -12,6 +12,9 @@ var app = express();
 console.log("Hello World");
 
 /** 2) A first working Express Server */
+app.get('/root', function(req, res) {
+ res.send('Hello Express')
+});
 
 
 /** 3) Serve an HTML file */
@@ -24,8 +27,8 @@ console.log("Hello World");
 
 
 /** 6) Use the .env file to configure the app */
- 
- 
+
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
@@ -39,7 +42,7 @@ console.log("Hello World");
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-  
+
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
@@ -50,7 +53,7 @@ console.log("Hello World");
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
-/** app.listen(process.env.PORT || 3000 ); */
+// app.listen(process.env.PORT || 3000 );
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
